a960958dc46718445c2f28c8b9b44435ade19579
Azkar <azkar.moulana@gmail.com>
2018-07-14 01:29:13 +0530

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" type="text/css" href="../eattendance/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../eattendance/css/bootstrap.css">
+    
 
-echo "hello azkar";
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+</head>
+<body class="body">
+    
+    <div>
+
+        <div >
+                <img class="mainlogo" src="../eattendance/images/Logo 2.png" alt="">
+
+        </div>
+
+        <div class="container para1">
+        <p>
+                NSBM Green University Town is the first ever green university in South Asia and sets an example for the whole South Asia by paving the way for environmental sustainability. The university is open for both national and international student community and it has turned a new chapter in Sri Lankan higher education.
+
+                NSBM Green University Town is established under the Ministry of Skills Development and Vocational Training and it is renowned for its world-class academic offerings. This state-of-the-art university offers nationally and internationally recognized, UGC approved degree programmes and foreign degree programmes in three faculties: Management, Computing and Engineering.
+        </p>
+
+        <div class="home-form">
+            <p>Check your eligibility :</p>
+            <form action="">
+                <input type="text" class="form-control " placeholder="Enter your ID here     Ex : 1000383"> <br>
+                <button type="submit" class="btn btn-outline-primary">Check</button>
+            </form>
+        </div>
+
+
+
+
+
+
+        </div>
+        
+    </div>
+
+</body>
+</html>
